6386b3c3b10385b2f3347c5205da931f108038fc
Adel Mahmoud <dolla2924@gmail.com>
2025-10-08 19:34:39 +0300

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
+const bottonID =document.getElementById("botton1") ; 
 const btnID1 =document.getElementById("btn1") ; 
-const btnID2 =document.getElementById("btn2") ; 
-const btnID3 =document.getElementById("btn3") ; 
-    btnID2.style.display="none";
-
-btnID1.addEventListener('click',function(){
     btnID1.style.display="none";
-    btnID2.style.display="block";
-    btnID3.style.display="none";
 
+bottonID.addEventListener('click',function(){
+    bottonID.style.display="none";
+    btnID1.style.display="block";
 })
